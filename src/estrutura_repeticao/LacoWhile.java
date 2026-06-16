4dc106563e7e6c3bff364d917cceae0647f5b9f7
gabrieljhon2004 <gabrieljhonatan3@gmail.com>
2026-06-16 18:26:45 -0300

    Fazendo alteracoes no LacoWhile

--- src/estrutura_repeticao/LacoWhile.java
+++ src/estrutura_repeticao/LacoWhile.java
@@ -11,51 +11,14 @@ public class LacoWhile {
             String nome = input.nextLine();
             System.out.println("Digite outro nome: (Caso deseje sair, Digite 'x')");
             String nome2 = input.nextLine();
-            if (nome.equals("x")){
+            if (nome.equals("x")) {
                 break;
-            }
-            else if (nome2.equals("x")){
+            } else if (nome2.equals("x")) {
                 break;
             }
 
         }
-            System.out.println("O programa foi encerrado");
+        System.out.println("O programa foi encerrado");
 
-    }
-
-    public static class CalculadoraSimples {
-
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-
-           System.out.println("digite qualquer número decimal: ");
-           double n1 = input.nextDouble();
-
-
-           System.out.println("Agora digite outro número decimal: ");
-           double n2 = input.nextDouble();
-
-            System.out.println("Escolha um operador matemático");
-            String operador = input.next();
-
-           switch (operador) {
-               case "+":
-                   System.out.println("O resultado é: " + (n1 + n2));
-                   break;
-               case "-":
-                   System.out.println("O resultado é: " + (n1 - n2));
-                   break;
-               case "*":
-                   System.out.println("O resultado é: " + (n1 * n2));
-                   break;
-                   case "/":
-                   System.out.println("O resultado é: " + (n1 / n2));
-                   break;
-
-               default: System.out.println("Digite um operador valido (+, -, *, /)");
-
-           }
-            input.close();
-        }
     }
 }
