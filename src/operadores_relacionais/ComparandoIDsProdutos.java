b1d68ba0e23a862199c100d3c622edf4fc71c8d9
gabrieljhon2004 <gabrieljhonatan3@gmail.com>
2026-06-22 02:59:06 -0300

    Adicionando pasta 'operadoresrelacionais' e arquivo 'ComparandoIDsProduto'

--- src/operadores_relacionais/ComparandoIDsProdutos.java
+++ src/operadores_relacionais/ComparandoIDsProdutos.java
@@ -1,4 +1,15 @@
 package operadores_relacionais;
 
-public class C {
+public class ComparandoIDsProdutos {
+    public static void main (String [] args) {
+
+        int idProdutoEstoque = 404;
+        int idProdutoCarrinho = 404;
+
+        if (idProdutoEstoque == idProdutoCarrinho) {
+            System.out.println("O produto do carrinho é o mesmo do estoque. Código: \" + idProdutoEstoque");
+        }
+
+
+    }
 }
