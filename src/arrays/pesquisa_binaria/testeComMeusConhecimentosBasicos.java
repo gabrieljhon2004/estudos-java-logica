5e82922a6f98b251934d6e40ec80f1be58204305
gabrieljhon2004 <gabrieljhonatan3@gmail.com>
2026-08-09 19:05:44 -0300

    Adicionando pasta pesquisabinaria

---
+++ src/arrays/pesquisa_binaria/testeComMeusConhecimentosBasicos.java
@@ -0,0 +1,19 @@
+package pesquisa_binaria;
+import java.util.Scanner;
+
+
+public class testeComMeusConhecimentosBasicos {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        
+
+
+
+
+
+
+
+        input.close();
+    }
+}
